5a5fc2005f5b90c23d49121cab53651ac15f18d7
Anuradha Dilshan <anuradha256kcc@gmail.com>
2025-04-17 01:43:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtHgnVsamgf76y3cE-1OBFNmGECbcbhsE",
-  authDomain: "lrdc-boat-app.firebaseapp.com",
-  projectId: "lrdc-boat-app",
-  storageBucket: "lrdc-boat-app.appspot.com",
-  messagingSenderId: "820531552490",
-  appId: "1:820531552490:web:a144a3e7e3ccac6cbab793",
-  measurementId: "G-7KGWS52JJV",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
